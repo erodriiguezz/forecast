6c6c06aee50fdd214a0b5c2bcaaafc1253d6c938
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2020-02-20 00:03:53 -0500

    worked on styling, and added new api

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,48 +1,61 @@
-// information to display
-// date
-// temperature
-// wind speed
-
 $(document).ready(function() {
   var apiKey = "9307e3c5c3422ccfbc04aa295f0ba123"; //openweather api key
 
   var city = "";
 
-  // on search button click ...
+  // on search button click search weather information
   $(".search-bttn").click(() => {
-    var userInput = $(".city-input").val();
+    handleSearch();
+  });
 
-    // if user typed in the city ...
-    if (userInput) {
-      city = userInput; // pass the user input to city
-
-      //handle screens
-      $(".alert").hide();
-      $(".search-view").hide();
-      $(".detailed-view").show();
-
-      // get information from api using user input
-      var queryURL =
-        "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=" +
-        city +
-        "&appid=" +
-        apiKey +
-        "";
-
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(object => {
-        var temperature = object.main.temp;
-        var windSpeed = object.wind.speed;
-        console.log(object);
-
-        $(".city").text(city);
-        $(".temperature").text("" + temperature + " Fahrenheit");
-        $(".wind-speed").text("" + windSpeed + " MPH");
-      });
-    } else {
-      $(".alert").show();
+  // on enter search weather information
+  $(".city-input").on("keyup", event => {
+    if (event.keyCode === 13) {
+      handleSearch();
     }
   });
+
+  function handleSearch() {
+    city = $(".city-input").val(); // add value to city
+
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" +
+      city +
+      "&appid=" +
+      apiKey +
+      "";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      success: onSuccess,
+      error: function() {
+        $(".alert").removeClass("fade");
+        $(".alert").addClass("show");
+
+        // hide alert afer two seconds
+        setTimeout(function() {
+          $(".alert").removeClass("show");
+          $(".alert").addClass("fade");
+        }, 1500);
+      }
+    });
+  }
+
+  function onSuccess(object) {
+    //handle screens
+    $(".alert").hide();
+    $(".search-view").hide();
+    $(".detailed-view").show();
+
+    // informtion from api
+    console.log(object);
+
+    // populate screen
+    // $(".city").text(city);
+    // $(".temperature").text("" + temperature + " Fahrenheit");
+    // $(".wind-speed").text("" + windSpeed + " MPH");
+  }
+
+  function onError() {}
 });
